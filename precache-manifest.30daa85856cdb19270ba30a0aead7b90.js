439a5a59520d67c5ac66fc4bf8324fdb4099ff86
Carlos Jara <cjjara002@gmail.com>
2020-03-16 16:21:30 -0700

    Updates

---
+++ precache-manifest.30daa85856cdb19270ba30a0aead7b90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d8b324a3d9651b5e1399ac8da1a09f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "02709947a0a60ba822d5",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6f03cc7d60761ce4a557",
+    "url": "/portfolio/static/css/main.5b94c17b.chunk.css"
+  },
+  {
+    "revision": "02709947a0a60ba822d5",
+    "url": "/portfolio/static/js/2.6855ad56.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.6855ad56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f03cc7d60761ce4a557",
+    "url": "/portfolio/static/js/main.ce414df6.chunk.js"
+  },
+  {
+    "revision": "54d0aef375d405e7b588",
+    "url": "/portfolio/static/js/runtime-main.a41aaf7c.js"
+  }
+]);
